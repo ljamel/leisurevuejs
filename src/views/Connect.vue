a0e7c3f524a5291e9fd2c08a442c184970b6f310
djamal lamri <contact@progfacil.com>
2020-12-01 23:00:46 +0100

    first commit for leisure

---
+++ src/views/Connect.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+
+    <link href="./src/assets/styles.css" rel="stylesheet">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<style>
+@import '../assets/styles.css';
+</style>
