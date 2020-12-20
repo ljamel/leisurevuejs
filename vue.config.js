a0b476ef4d7f8d4279a6f9906bd7a142e22fd7e9
djamal lamri <contact@progfacil.com>
2020-12-20 20:19:09 +0100

    config for prod

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devServer: {
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        port: 8080,
+        public: 'cadito.fr:8080',
+    },
+    publicPath: '/'
+}
